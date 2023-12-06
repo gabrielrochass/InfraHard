ed703c06813d9b40ced347696f5741750002a884
Gabriel Rocha <120466775+gabrielrochass@users.noreply.github.com>
2023-12-06 16:09:17 -0300

    versão final

--- q7.asm
+++ q7.asm
@@ -1,99 +1,97 @@
-# Definindo as variáveis
+# variáveis para armazenar valores
 .data
-dividendo:  .word 0          # Armazena o dividendo
-divisor:    .word 0          # Armazena o divisor
-RESULT:     .word 0          # Armazena o resultado (quociente)
-input_msg:  .asciiz "Digite o dividendo: "
-input_msg2: .asciiz "Digite o divisor: "
-newline:    .asciiz "\n"
+dividendo:  .word 0          # armazena o dividendo
+divisor:    .word 0          # armazena o divisor
+result:     .word 0          # armazena o resultado (quociente)
+input_msg:  .asciiz "Digite o dividendo: "# msg de input 1
+input_msg2: .asciiz "Digite o divisor: " # msg de input 2
+newline:    .asciiz "\n" # printa quebra de linha
 
 .text
 .globl main
 
 main:
-    # Mensagem para entrada do dividendo
+    # solicita e recebe o dividendo
     li $v0, 4
     la $a0, input_msg
     syscall
 
-    # Recebendo o dividendo
     li $v0, 5
     syscall
-    move $t0, $v0  # Armazena o dividendo em $t0
+    move $t0, $v0  # armazena o dividendo
 
-    # Mensagem para entrada do divisor
+    # solicita e recebe o divisor
     li $v0, 4
     la $a0, input_msg2
     syscall
 
-    # Recebendo o divisor
     li $v0, 5
     syscall
-    move $t1, $v0  # Armazena o divisor em $t1
+    move $t1, $v0  # armazena o divisor
 
-    li $t2, 0       # Inicializa o registrador $t2 para o quociente
-    li $t3, 0       # Inicializa o registrador $t3 para o resto
+    # inicializa o registrador para quociente
+    li $t2, 0
+    # li $t3, 0
 
-    # Verifica se o dividendo é zero
-    beq $t0, $zero, zero_dividend
-    # Verifica se o divisor é zero
-    beq $t1, $zero, error_divisor
+    # verifica condições e executa a divisão
+    beq $t0, $zero, store_values  # se dividendo = 0, encerra printando 0 e pula para sair
+    beq $t1, $zero, error_divisor # se divisor = 0, printa erro e sai
 
-    # Verifica se o dividendo é negativo
-    blt $t0, $zero, negativo
-    positivo:
-        loop_positivo:
-            sub $t0, $t0, $t1  # Subtrai o divisor do dividendo
-            bge $t0, $zero, increment_positivo  # Se o dividendo >= 0, incrementa o quociente
-            j exit_positivo
-        increment_positivo:
-            addi $t2, $t2, 1  # Incrementa o quociente
-            j loop_positivo
-    negativo:
-        neg $t0, $t0  # Transforma o dividendo em positivo
-        loop_negativo:
-            sub $t0, $t0, $t1  # Subtrai o divisor do dividendo
-            bge $t0, $zero, increment_negativo  # Se o dividendo >= 0, incrementa o quociente
-            j exit_negativo
-        increment_negativo:
-            addi $t2, $t2, 1  # Incrementa o quociente
-            j loop_negativo
+    blt $t0, $zero, negative_dividend  # se dividendo < 0, executa divisão para números negativos
 
-    exit_positivo:
+    positive_dividend:
+        loop_positive:
+            sub $t0, $t0, $t1  # subtrai o divisor do dividendo
+            bge $t0, $zero, increment_positive  # se o dividendo >= 0, incrementa o quociente
+            j exit_positive
+
+        increment_positive:
+            addi $t2, $t2, 1  # incrementa o quociente
+            j loop_positive
+
+    negative_dividend:
+        neg $t0, $t0
+        loop_negative:
+            sub $t0, $t0, $t1  # subtrai o divisor do dividendo
+            bge $t0, $zero, increment_negative  # se o dividendo >= 0, incrementa o quociente
+            j exit_negative
+
+        increment_negative:
+            addi $t2, $t2, 1  # incrementa o quociente
+            j loop_negative
+
+    exit_positive:
         j store_values
-    exit_negativo:
-        neg $t2, $t2  # Transforma o quociente em negativo
-        j store_values
 
-    zero_dividend:
+    exit_negative:
+        neg $t2, $t2  # transforma o quociente em negativo
         j store_values
 
     error_divisor:
-        la $a0, error_message  # Carrega o endereço da mensagem de erro em $a0
-        li $v0, 4              # Código da syscall para imprimir string
+        la $a0, error_message  # carrega mensagem de erro
+        li $v0, 4              # printa string
         syscall
         j exit_program
 
     store_values:
-        move $t3, $t0          # Move o valor final do dividendo para $t3 (resto)
-        sw $t2, RESULT         # Armazena o quociente na variável RESULT
-        sw $t3, REMAINDER      # Armazena o resto na variável REMAINDER
-
-        # Imprime os valores do quociente e do resto
-        lw $a0, RESULT         # Carrega o quociente para imprimir
-        li $v0, 1              # Código da syscall para imprimir inteiro
+        sw $t2, result  # armazena o quociente na variável RESULT
+        
+        # printa o quociente
+        lw $a0, result
+        li $v0, 1
         syscall
 
-        li $v0, 4              # Código da syscall para imprimir string
-        la $a0, newline        # Carrega a quebra de linha
+        # printa quebra de linha
+        li $v0, 4
+        la $a0, newline
         syscall
 
-        j exit_program
+        j exit_program  # finaliza o programa
 
     exit_program:
-        li $v0, 10            # Código da syscall para sair do programa
+        li $v0, 10  # Syscall para encerrar o programa
         syscall
 
     # Dados
     .data
-    error_message: .asciiz "Erro: divisão por zero"
+    error_message: .asciiz "Erro: divisão por zero"  # msg de erro
